b42758516a9d3ed4d25d271a6b7c22ffcd13d787
Roman <roman.sergeevich35@gmail.com>
2023-01-26 14:44:52 +0300

    pobeda

---
+++ db/seeders/20230126073706-demo-user.js
@@ -0,0 +1,25 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Themes', [{
+      title: 'Города',
+      background: 'https://upload.wikimedia.org/wikipedia/ru/thumb/3/3b/FC_Spartak_Moscow_Logotype.svg/1200px-FC_Spartak_Moscow_Logotype.svg.png',
+      userId: 1,
+    },
+    {
+      title: 'Птицы',
+      background: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/FC_Zenit_1_star_2015_logo.svg/1200px-FC_Zenit_1_star_2015_logo.svg.png',
+      userId: 2,
+    },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
